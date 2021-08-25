f1ca5e1e943b484884784c6c148114e820754ef6
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-08-25 16:10:22 -0500

    created rolesRoutes and wrote routes; referenced rolesRoutes.js in index.js

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use(require('./departmentRoutes'));
 router.use(require('./employeesRoutes'));
+router.use(require('./rolesRoutes'));
 
 module.exports = router;
